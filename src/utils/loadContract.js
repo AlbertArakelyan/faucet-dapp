47989e01a38700f5922ca97e03e7d2aeea4dbd6b
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-11-26 20:41:34 +0400

    added functionality for adding contracts

---
+++ src/utils/loadContract.js
@@ -0,0 +1,10 @@
+import contract from '@truffle/contract';
+
+export const loadContract = async (name) => {
+  const res = await fetch(`/contracts/${name}.json`);
+  const Artifact = await res.json();
+
+  return {
+    contract: contract(Artifact),
+  };
+};
